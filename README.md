d9a4d80f1a3c6afea10c06a24488a20ff934177f
jessamynsmith <geekchick77@gmail.com>
2022-06-01 18:20:49 -0600

    Initial commit of download script

---
+++ README.md
@@ -0,0 +1,17 @@
+# google-presentation-thumbnails-appscript
+
+### Development
+
+Fork the project on github and git clone your fork, e.g.:
+
+    git clone https://github.com/<username>/google-presentation-thumbnails-appscript.git
+    
+You will need a Google Cloud Project with the following:
+
+    - Google Slides API enabled
+    - A Google presentation which has granted access to the generated email on the service account
+
+Edit the script to set the appropriate values for const IMAGE_SIZE, MAX_SLIDE_COUNT, PRESENTATION_ID, and FOLDER_ID.
+
+Copy and paste the contents of get_slide_thumbnails.js into Google Apps Script
+Run the script
---
+++ get_slide_thumbnails.js
@@ -0,0 +1,58 @@
+const SIZE = {
+  UNSPECIFIED: 'THUMBNAIL_SIZE_UNSPECIFIED',
+  LARGE: 'LARGE',
+  MEDIUM: 'MEDIUM',
+  SMALL: 'SMALL',
+};
+
+const IMAGE_SIZE = SIZE.SMALL;
+const MAX_SLIDE_COUNT = 1;
+const PRESENTATION_ID = '<PRESENTATION_ID_FROM_GOOGLE_URL>';
+const FOLDER_ID = '<FOLDER_ID_FROM_GOOGLE_URL>';
+
+
+function mainFunction() {
+
+  const getThumbnailUrl = (presentationId, objectId) => {
+    let thumbnail = Slides.Presentations.Pages.getThumbnail(
+        presentationId,
+        objectId,
+        {
+          'thumbnailProperties.mimeType': 'PNG',
+          'thumbnailProperties.thumbnailSize': IMAGE_SIZE,
+        }
+      );
+    return thumbnail.contentUrl;
+  };
+
+
+  const getImageLinks = (presentationId) => {
+
+    const presentation = Slides.Presentations.get(PRESENTATION_ID);
+    const slides = presentation.slides;
+
+    let thumbnailUrls = [];
+    for ( let i = 0; i < MAX_SLIDE_COUNT; i++) {
+      let objectId = slides[i].objectId;
+      let thumbnailUrl = getThumbnailUrl(presentationId, objectId);
+      thumbnailUrls.push({
+        objectId: objectId,
+        thumbnailUrl: thumbnailUrl
+      });
+    }
+    return thumbnailUrls;
+  };
+
+
+  let imageLinks = getImageLinks(PRESENTATION_ID);
+
+  for ( let i = 0; i < imageLinks.length; i++) {
+    let blob = UrlFetchApp.fetch(
+        imageLinks[i].thumbnailUrl
+      )
+        .getBlob()
+        .setName(`page${("000" + (i + 1)).slice(-3)}.png`);
+    DriveApp.getFolderById(FOLDER_ID).createFile(blob);
+  }
+  
+}
