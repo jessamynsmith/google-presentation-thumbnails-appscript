0c1056132179b5a8d1c9751f300f7b0a54f55c22
jessamynsmith <geekchick77@gmail.com>
2022-06-09 16:31:40 -0600

    Added code to find font size

--- get_slide_thumbnails.js
+++ get_slide_thumbnails.js
@@ -7,8 +7,8 @@ const SIZE = {
 
 const IMAGE_SIZE = SIZE.SMALL;
 const MAX_SLIDE_COUNT = 1;
-const PRESENTATION_ID = '<PRESENTATION_ID_FROM_GOOGLE_URL>';
-const FOLDER_ID = '<FOLDER_ID_FROM_GOOGLE_URL>';
+const PRESENTATION_ID = '1rPb9jfzOsTwmGYXM4nBnjuE51dcXexYaNvu9KPSQEVQ';
+const FOLDER_ID = '1iCG9Qu0JLFx2b9O1ohROTyMo-95FqD4K';
 
 
 function mainFunction() {
@@ -27,12 +27,16 @@ function mainFunction() {
 
 
   const getImageLinks = (presentationId) => {
+    let presentation2 = SlidesApp.openById(presentationId);
 
     const presentation = Slides.Presentations.get(presentationId);
     const slides = presentation.slides;
 
     let thumbnailUrls = [];
     for ( let i = 0; i < MAX_SLIDE_COUNT; i++) {
+      fixFont(presentation2.getSlides()[i], 'title', 'short stuf');
+      //fixFont(presentation2.getSlides()[i], 'title', 'this is a pretty long string it probably will not fit');
+      //fixFont(presentation2.getSlides()[i], 'title', 'this is a medium long string');
       let objectId = slides[i].objectId;
       let thumbnailUrl = getThumbnailUrl(presentationId, objectId);
       thumbnailUrls.push({
@@ -42,6 +46,75 @@ function mainFunction() {
     }
     return thumbnailUrls;
   };
+
+
+  let approxChars = (width, height, fontsize) => {
+    let numRows = Math.trunc(height / (fontsize * 1.2));
+    let charsPerRow = Math.trunc((width / fontsize) / 0.6);
+    console.log('numRows: ' + numRows + ' charsPerRow: ' + charsPerRow)
+    let numChars = numRows.toFixed(0) * charsPerRow.toFixed(0);
+    return numChars;
+  }
+
+
+  let fixFont = (currentSlide, textPlaceholder, replacementText) => {
+
+    let fontSizes = [96, 72, 60, 48, 36, 30, 24];
+    
+    let listOfElements = currentSlide.getPageElements();
+    for (let i=0; i < listOfElements.length; i++) {
+
+      let objectElementType = listOfElements[i].getPageElementType();
+
+      // Filter step 1: filter for 'Shape' element type since textboxes are designated as shapes
+        if(objectElementType.toString() == 'SHAPE') {
+          Logger.log('found shape')
+          let shape = listOfElements[i].asShape();
+          let objectText = shape.getText();
+
+          Logger.log(objectText.asString());
+        
+          // Filter step 2: filter for shape whose text matches the placeholder string (e.g., 'Title' == 'Title')
+          if(objectText.asString().trim() == textPlaceholder) {
+            let baselineObjectHeight = shape.getHeight().toFixed(2);
+            let baselineObjectWidth = shape.getWidth().toFixed(2);
+
+            Logger.log('Object Text is: '+ objectText.asString());
+            Logger.log('Baseline height is: '+ baselineObjectHeight);
+            Logger.log('Baseline width is: '+ baselineObjectWidth);
+
+            let textChars = replacementText.length;
+            let fontSize = 96;
+            for (let j = 0; j < fontSizes.length; j++) {
+              fontSize = fontSizes[j];
+              let numChars = approxChars(baselineObjectWidth, baselineObjectHeight, fontSize);
+              Logger.log('textChars: ' + textChars + ', numChars: ' + numChars);
+              if (numChars > textChars) {
+                break;
+              }
+            }
+            
+            // if (textChars > numChars) {
+              // let scaleFactor = numChars / textChars;
+              // Logger.log('scaleFactor: ' + scaleFactor);
+              // let newFontSize = scaleFactor * fontSize;
+              // //let newFontSize = 2872.01 - 2696.25 * Math.pow(textChars, 0.00926896);
+              // //let newFontSize = 113.597 - (0.670927 * textChars);
+              // Logger.log(newFontSize);
+              // newFontSize = newFontSize.toFixed(0);
+              // Logger.log(newFontSize);
+              objectText.setText('');
+              let insertedText = objectText.appendText(replacementText);
+              insertedText.getTextStyle().setFontSize(fontSize);
+
+            // }
+
+            break;
+          }
+        }
+    }
+
+}
 
 
   let imageLinks = getImageLinks(PRESENTATION_ID);
